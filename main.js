8d296c026ea85b42f03127222aa71d936aa324eb
hellensaandrade <hellensaandrade@gmail.com>
2025-04-07 02:12:03 +0000

    inicio da logica da porcentagem da gorjeta

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue(){
   bill = document.querySelector("#bill").valueAsNumber
 }
 function receiveNumberOfPeopleValue(){
     numberOfPeople = document.querySelector("#people")
+}
+function receiveTipPorcentageValue(value){
+ tipPercentage = value / 100
+
+ buttonSelected = document.querySelector('#button-${value}')
+ buttonSelected.classList.add("button-selected")
+")
 }
